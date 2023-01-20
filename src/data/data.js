0993fe99056f401d1cf336724753963bebef8728
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2023-01-21 00:42:11 +0100

    initial  commit

---
+++ src/data/data.js
@@ -0,0 +1,39 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+import GogeAfrica from '../assets/projects/goge-africa.jpeg'
+import FurnitureApp from '../assets/projects/furniture-app.jpeg'
+
+
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:GogeAfrica,
+        github:"https://github.com/olatunde1998/portfolio-react",
+        live:"https://goge-africaa.netlify.app/",
+    },
+    {
+        id:2,
+        name:"Next JS Application",
+        image:FurnitureApp,
+        github:"https://github.com/olatunde1998/furniture-app",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+
+]
