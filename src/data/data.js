3b2c5ea553bda73fb313dbb94fb1984f0e1b6c98
geodevcode <geodevinstitute@gmail.com>
2023-03-10 18:11:09 +0100

    update changes

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 import WorkIm from '../assets/projects/workImg.jpeg'
 import RealEst from '../assets/projects/realestate.jpg'
-import GogeAfrica from '../assets/projects/goge-africa.jpeg'
+import SignUp from '../assets/projects/signup.jpeg'
 import FurnitureApp from '../assets/projects/furniture-app.jpeg'
 
 
@@ -9,29 +9,29 @@ export const data=[
     {
         id:1,
         name:"React JS Application",
-        image:GogeAfrica,
-        github:"https://github.com/olatunde1998/portfolio-react",
-        live:"https://goge-africaa.netlify.app/",
+        image:SignUp,
+        github:"https://github.com/RuchiDeve",
+        live:"https://goge-afri.netlify.app/",
     },
     {
         id:2,
         name:"Next JS Application",
         image:FurnitureApp,
-        github:"https://github.com/olatunde1998/furniture-app",
+        github:"https://github.com/RuchiDeve",
         live:"",
     },
     {
         id:3,
         name:"React JS Application",
         image:WorkIm,
-        github:"",
+        github:"https://github.com/RuchiDeve",
         live:"",
     },
     {
         id:4,
         name:"React JS Application",
         image:RealEst,
-        github:"",
+        github:"https://github.com/RuchiDeve",
         live:"",
     },
 
